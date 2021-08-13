c6c29d16bf91d5b5d7efa51a17e7a539d73beff7
Bartłomiej Lipiec <bartek.lipiec2@gmail.com>
2021-08-13 11:42:35 +0200

    fix v2

--- src/main/java/com/barlipdev/dwyf/controller/AuthController.java
+++ src/main/java/com/barlipdev/dwyf/controller/AuthController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.websocket.server.PathParam;
 import java.io.IOException;
 
 @RestController
@@ -22,8 +23,8 @@ public class AuthController {
         return authService.login(loginData);
     }
 
-    @PostMapping("/login/v2/{email}&{password}")
-    public User loginV2(@PathVariable String email, @PathVariable String password){
+    @PostMapping("/login/v2")
+    public User loginV2(@PathParam("email") String email, @PathParam("password") String password){
         return authService.login(new LoginData(email,password));
     }
 
